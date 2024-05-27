abb720011a43dc4c9461dd33813769cc9ae6a507
MezaJose12 <jameza1@espe.edu.ec>
2024-05-27 17:00:53 -0500

    Codigo completo

--- SEMANA3/Persona.java
+++ SEMANA3/Persona.java
@@ -1,23 +1,46 @@
 import java.util.Scanner;
 public class Persona{
     public static void main(String[] args) {
+        Codigo objeto = new Codigo();
         Usuario objeto1 = new Usuario();
         Scanner leer = new Scanner(System.in);
         String nombre1,nombre2;
         String apellido;
         String idPersona;
+        
         System.out.println("Ingrese su primer nombre:");
         nombre1=leer.next();
+        
         System.out.println("Ingrese su segundo nombre:");
         nombre2=leer.next();
+        
         System.out.println("Ingrese el apellido:");
         apellido=leer.next();
+        
         System.out.println("Ingrese el idPersona:");
         idPersona=leer.next();
+        
         objeto1.setNombre1(nombre1);
         objeto1.setNombre2(nombre2);
         objeto1.setApellido(apellido);
-        System.out.println("Datos ingresados:"+objeto1.getNombre1());
-        System.out.println("Datos ingresados:"+objeto1.getNombre2());
+        objeto1.setIdPersona(idPersona);
+
+        String usuario;
+        usuario = generar(nombre1,nombre2,apellido);
+
+        System.out.println("Datos generados:");
+        System.out.println("Usuario:" +usuario);
+        System.out.println("Idpersona:"+objeto1.getIdPersona());
+
     }
+
+    private static String generar(String nombre1, String nombre2, String apellido) {
+
+        String inicialNombre1 = nombre1.substring(0, 1).toLowerCase();
+        String inicialNombre2 = nombre2.substring(0, 1).toLowerCase();
+        String apellidoMinuscula = apellido.toLowerCase();
+
+        return inicialNombre1 + inicialNombre2 + apellidoMinuscula;
+    }
+    
 }
